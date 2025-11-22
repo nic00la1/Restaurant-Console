9212eab4b22e657f2386d1a21ea5878eaae2626b
Nicola <nic00la.kaleta@gmail.com>
2025-11-22 19:50:17 +0100

    stworzone pliki klas

---
+++ Restaurant-Console/Classes/Waiter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_Console.Classes
+{
+    public class Waiter
+    {
+        // Napisz program, który pozwoli kelnerowi:
+        // 1. przyjmować zamówienia od klientów 
+        // 2. przekazywać zamówienia do kuchni
+        // 3. generować rachunki na podstawie zamówień
+    }
+}
